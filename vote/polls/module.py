3299829c505c34b069a0be00ba9c8f280210cf92
Rodolfo Blanco <rfblanco@uc.cl>
2016-02-27 18:40:53 -0300

    remove AI

--- vote/polls/module.py
+++ vote/polls/module.py
@@ -1,10 +1,15 @@
 import crypto
+import libnacl
 import string
 
 
 def salt(n=64):
     let = string.ascii_lowercase + string.ascii_uppercase + string.digits
     return ''.join(crypto.choice(let)for _ in range(n))
+
+
+def encrypt(key, message):
+    pass
 
 
 def hash(clave, salt):
